52516654411791177826d701c0973ecdba8db822
nurhidaylma <ilmanurhidayati752@gmail.com>
2020-10-09 13:30:37 +0700

    initial commit modul2

---
+++ MODUL2 ILMA/home.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+</head>
+<body>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-12" style="background-color:blue;">
+                <nav class="navbar navbar-expand-lg justify-content-center">                                        
+                    <div class="navbar-nav">
+                        <a class="nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
+                        <a class="nav-link" href="booking.php">Booking</a>                        
+                    </div>                    
+                </nav>
+            </div>            
+        </div>                    
+    </div>
+
+    <div class="container-fluid"> 
+        <div class="row justify-content-center">
+            <div class="col-md-12 ">
+                <h5>EAD HOTEL</h5>
+                <h5>Welcome to 5 Stars Hotel</h5>    
+            </div>
+        </div>
+
+        <div class="row justify-content-center">
+            <div class="col-md-5 text-center" >                
+                <div class="card" style="width: 18rem; margin-left: 200px">
+                    <img src="assets\standard.jfif" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 >Standard</h5>
+                        <h5 >$ 90/Day</h5>
+                        <p class="card-header">Facilities</p>                        
+                        <ul class="list-group list-group-flush">
+                            <li class="list-group-item">1 Single Bed</li>
+                            <li class="list-group-item">1 Bathroom</li>                            
+                        </ul>                                                
+                    </div>  
+                    <div class="card-footer">
+                        <a href="#" class="btn btn-primary">Book Now</a>                                       
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-2 text-center">
+                <div class="card" style="width: 18rem;">
+                    <img src="assets\superior.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 >Superior</h5>
+                        <h5 >$ 150/Day</h5>
+                        <p class="card-header">Facilities</p>                        
+                        <ul class="list-group list-group-flush">
+                            <li class="list-group-item">1 Double Bed</li>
+                            <li class="list-group-item">1 Television and Wi-Fi</li>
+                            <li class="list-group-item">1 Bathroom with hot water</li>                            
+                        </ul>                                                
+                    </div>  
+                    <div class="card-footer">
+                        <a href="#" class="btn btn-primary">Book Now</a>                                       
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-5 text-center">
+                <div class="card" style="width: 18rem;">
+                    <img src="assets\luxury.jfif" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 >Luxury</h5>
+                        <h5 >$ 200/Day</h5>
+                        <p class="card-header">Facilities</p>                        
+                        <ul class="list-group list-group-flush">
+                            <li class="list-group-item">2 Double Bed</li>
+                            <li class="list-group-item">2 Bathroom with hot water</li>                                 
+                            <li class="list-group-item">1 Kitchen</li>                                 
+                            <li class="list-group-item">1 Television and Wi-Fi</li>
+                            <li class="list-group-item">1 Workroom</li>                                                                 
+                        </ul>                                                
+                    </div>  
+                    <div class="card-footer">
+                        <a href="#" class="btn btn-primary">Book Now</a>                                       
+                    </div>
+                </div>               
+            </div>                   
+        </div>
+    </div>
+</body>
+</html>
