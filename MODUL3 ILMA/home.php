0c0e5bc7359691449154c0856d3de607395d3515
nurhidaylma <ilmanurhidayati752@gmail.com>
2020-11-13 14:43:46 +0700

    init commit

---
+++ MODUL3 ILMA/home.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="assets\style.css">
+</head>
+<body>        
+    <nav class="navbar navbar-light bg-light">
+        <a class="navbar-brand">EAD Events</a>
+        <form class="form-inline">
+            <a href="" class="p-2">Home</a>    
+            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Buat Event</button>
+        </form>
+    </nav>
+      
+</body>
+</html>
