ba0429f0d55c7b2af98d1de782790ca26cf58e99
nurhidaylma <ilmanurhidayati752@gmail.com>
2020-11-19 22:02:29 +0700

    add crud delete

--- MODUL3 ILMA/delete.php
+++ MODUL3 ILMA/delete.php
@@ -2,7 +2,7 @@
     include 'config.php';
     $id_event = $_GET['id'];
 
-    $query = "DELETE FROM event_table";
+    $query = "DELETE FROM event_table WHERE id = '$id_event'";
     $delete = mysqli_query($conn, $query);
 
     header("Location:home.php")
