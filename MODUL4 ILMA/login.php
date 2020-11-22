2ac35e5b8cb1c3b6ae6de2f6cc86b940de703936
nurhidaylma <ilmanurhidayati752@gmail.com>
2020-11-22 23:18:52 +0700

    add login page

--- MODUL4 ILMA/login.php
+++ MODUL4 ILMA/login.php
@@ -14,9 +14,50 @@
         <a class="navbar-brand text-secondary">WAD Beauty</a>
         <form class="form-inline">
             <a href="#" class="text-secondary p-2">Login</a>    
-            <a href="register.php" class="p-2 text-secondary" type="submit">Register</a>
+            <a href="form_register.php" class="p-2 text-secondary" type="submit">Register</a>
         </form>
     </nav>
+
+    <?php        
+        if(isset($_POST['btnLogin'])){
+            include('config.php');
+
+            $email = $_POST['inputEmail'];
+            $pass = $_POST['inputPass'];               
+
+            //cek email apakah sudah terdaftar
+            $cek_email = mysqli_query($conn, "SELECT * FROM user WHERE email = '$email'");
+            $data_user = $cek_email->fetch_array();
+
+            //cek password
+            $cek_password = mysqli_query($conn, "SELECT * FROM user WHERE email = '$email' and password = '$pass'");            
+
+            if (mysqli_num_rows($cek_email) == 0){
+                echo "<div class='alert alert-warning' role='alert'>
+                        Email belum terdaftar!
+                    </div>";
+            } elseif (mysqli_num_rows($cek_email) == 1){                
+                if($cek_password){                    
+                    session_start();
+                    $_SESSION['email'] = $email;
+                    $_SESSION['password'] = $pass;
+                    $_SESSION['id'] = $data_user['id'];
+                    $_SESSION['nama'] = $data_user['nama'];
+                    //cookie
+                    if(isset($_POST['chRemember'])) {                        
+                        setcookie ('email', $email, time()+ 3600);
+                        setcookie ('password', $pass, time()+ 3600);                                                
+                    }                    
+                    header("Location: index.php");
+                } else {
+                    echo "<div class='alert alert-warning' role='alert'>
+                        Email atau kata sandi anda salah!
+                    </div>";                                        
+                }
+            }            
+        }
+        
+    ?>
 
     <!-- konten -->  
     <div class="mt-4 d-flex justify-content-center">    
@@ -24,41 +65,30 @@
             <div class="card-body px-5">            
                 <h5 class="card-title text-center">Login</h5>        
                 <hr>
-                <form method="post" action="register.php">                    
+                <form method="post" action="">                    
                     <div class="form-group">
                         <label for="inputEmail">E-mail</label>
-                        <input type="email" class="form-control" id="inputEmail" name="inputEmail" required>
+                        <input type="email" class="form-control" id="inputEmail" name="inputEmail" required
+                            value="<?php if(isset($_COOKIE['email'])) { echo $_COOKIE['email']; } ?>"
+                        >
                     </div>                    
                     <div class="form-group">
                         <label for="inputPass">Kata Sandi</label>
-                        <input type="password" class="form-control" id="inputPass" name="inputPass" required>
-                    </div>                    
+                        <input type="password" class="form-control" id="inputPass" name="inputPass" required
+                            value="<?php if(isset($_COOKIE['password'])) { echo $_COOKIE['password']; } ?>"
+                        >
+                    </div>                     
                     <div class="form-group form-check">
-                        <input type="checkbox" class="form-check-input" id="chRemember">
+                        <input type="checkbox" class="form-check-input" name="chRemember" id="chRemember">
                         <label class="form-check-label" for="chRemember">Remember me</label>
                     </div>
                     <div class="d-flex justify-content-center">
                         <button type="submit" class="btn btn-primary" name="btnLogin">Login</button>
                     </div>
-                    <p class="text-center p-2"> Belum punya akun? <a href="register.php">Registrasi</a> </p>
+                    <p class="text-center p-2"> Belum punya akun? <a href="form_register.php">Registrasi</a> </p>
                 </form>            
             </div>
         </div>
     </div>   
-
-    <?php
-        if(isset($_POST['btnLogin'])){
-            include('config.php');
-
-            $email = $_POST['inputEmail'];
-            $pass = $_POST['inputPass'];
-
-            $query = "SELECT * FROM user WHERE email = '$email' and password = $pass";
-            $result = mysqli_query($conn, $query);
-
-            
-        }
-        
-    ?>
 </body>
 </html>
