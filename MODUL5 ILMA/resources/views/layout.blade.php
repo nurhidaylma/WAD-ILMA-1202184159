8bf6b8d40641c432eb72ba2d5a251938e9859571
nurhidaylma <ilmanurhidayati752@gmail.com>
2020-12-06 17:57:00 +0700

    add layout root

--- MODUL5 ILMA/resources/views/layout.blade.php
+++ MODUL5 ILMA/resources/views/layout.blade.php
@@ -24,7 +24,7 @@
             </li>
         </ul>
 
-        <div class="container d-flex justify-content-center pt-3">
+        <div class="container text-center pt-5">
             @yield('content')
         </div>
     </div>
